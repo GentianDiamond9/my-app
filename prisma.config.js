907ecd3c3b3bfee66e0835b9945bebb19e33b5e9
GentianDiamond9 <scientistkey1025@keio.jp>
2026-07-08 16:27:57 +0900

    Add datasource url to prisma config for migrate deploy

--- prisma.config.js
+++ prisma.config.js
@@ -1,4 +1,8 @@
 // prisma.config.js
 export default {
   schema: "prisma/schema.prisma",
+  // 本番の migrate deploy コマンドが DATABASE_URL を見つけられるように設定を追加するのじゃ
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
 };
